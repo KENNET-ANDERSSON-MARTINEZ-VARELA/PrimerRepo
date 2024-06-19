09831a3838a5a9be8fb83b2bf03a2d5259e9dd2b
KENNET-ANDERSSON-MARTINEZ-VARELA <kennetmartinz16@gmail.com>
2024-06-18 23:29:40 -0600

    Tarea en clase, Kennet Martinez

---
+++ Ejercicio_diccionario.py
@@ -0,0 +1,9 @@
+diccionario = [
+    {"Numero": 1, "nombre": "Pablo", "edad": 25},
+    {"Numero": 2, "nombre": "Miguel", "edad": 30},
+    {"Numero": 3, "nombre": "Luis", "edad": 35},
+    {"Numero": 4, "nombre": "Carlos", "edad": 45},
+]
+
+for buscar in diccionario:
+    print(buscar)
